9a230d8213a4b6459eb635b4a29864d387a81a73
Jorge <jorgemarpa@ug.uchile.cl>
2021-10-01 16:18:44 -0700

    new mpi dep, mpi and cuda instructions to readme, run mpi script

--- README.md
+++ README.md
@@ -13,3 +13,106 @@
 * loading and saving
 * add radial spline dm
 * add cholesky batch solve
+
+# MPI instructions
+
+`scatterbrain` includes the option to use MPI to do parallel IO of FITS files. This feature is only available when computing new background models from TESS full frame images using the `run_backdrop_mpi.py` script. Note that only IO is done in parallel, other functionalities (e.g. BackDrop) run in a single CPU.
+
+## Installation
+
+#### OpenMPI
+This package uses `mpi4py` library to interface Python code with MPI. You will need a working version of MPI, for example OpenMPI. Here are basic instructions of how to install OpenMPI on a OSX machine with [Home Brew](https://formulae.brew.sh/formula/open-mpi):
+
+```
+brew install openmpi
+```
+This will enable the `mpirun` and `mpiexec` commands. To test it, use the following `hello.c` example
+
+```c
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    int rank;
+    int world;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world);
+    printf("Hello: rank %d, world: %d\n",rank, world);
+    MPI_Finalize();
+}
+```
+Compile and the run the script with `mpirun`:
+
+```sh
+foo@bar:~$ mpicc -o hello ./hello.c
+foo@bar:~$ mpiexec -n 2 ./hello
+Hello: rank 0, world: 2
+Hello: rank 1, world: 2
+```
+
+#### MPI for Python
+
+Then install `mpi4py` following the [documentation](https://mpi4py.readthedocs.io/en/stable/install.html). Using PyPI:
+
+```
+pip install mpi4py
+```
+To test it:
+
+```sh
+foo@bar:~$ mpiexec -n 5 python -m mpi4py.bench helloworld
+Hello, World! I am process 0 of 5 on localhost.
+Hello, World! I am process 1 of 5 on localhost.
+Hello, World! I am process 2 of 5 on localhost.
+Hello, World! I am process 3 of 5 on localhost.
+Hello, World! I am process 4 of 5 on localhost.
+```
+
+Now you are ready to run `scatterbrain` with MPI.
+
+## Usage
+
+To run one of the `run_backdrop*.py` scripts that fit background models to TESS FFIs using MPI use the following command on your local terminal:
+
+```sh
+foo@bar:~$ mpiexec -n 4 python run_backdrop_mpi.py [program arguments]
+```
+where the `-n 4` flag sets the number of ranks to be used.
+
+# CUDA instructions
+
+## Installation
+`scatterbrain` can also run in GPU hardware for maximum speed up! For this, we use [CuPy](https://docs.cupy.dev/en/stable/) library that implements NumPy multi-dimensional arrays on CUDA.
+In order to run `scatterbrain` in a GPU you will need:
+  * NVIDIA GPU with CUDA
+  * CUDA Toolkit according to your GPU hardware (e.g. v11.0)
+  * CuPy version according to your CUDA Toolkit version
+
+Follow [CuPy instructions](https://docs.cupy.dev/en/stable/install.html) to install it.
+We recommend using the `pip` command, as example for CUDA v11.0:
+
+```
+pip install cupy-cuda110
+```
+
+## Usage
+
+To import CuPy instead of NumPy you just need to set a new environmental variable `USE_CUPY` before importing `scatterbrain` modules so all of them can import the right packages. For default (also if `USE_CUPY == False`) `scatterbrain` imports only NumPy.
+
+```python
+import os
+
+os.environ["USE_CUPY"] = str(True)
+import scatterbrain
+```
+
+We opted for the use of an environmental variable, instead of just checking for GPU availability, in the weird case you want to run `scatterbrain` CPU-only on a GPU-available machine.
+
+If using one of the `run_backdrop*.py` scripts that fit background models to TESS FFIs, you just have to add the `--cupy` flag to enable the GPU, this will automatically set the environmental variable.
+
+To fit new FFI-background model you can combine the power of parallel IO and GPU computing by using the following command:
+
+```sh
+mpiexec -n 4 python run_backdrop_mpi.py --cupy [program arguments]
+```
--- src/run_backdrop_mpi.py
+++ src/run_backdrop_mpi.py
@@ -7,50 +7,59 @@ import os
 import sys
 import time
 
-import cupy as cp
-import numpy as np
-
 log = logging.getLogger(__name__)
-
+# image size
 N = 2048
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--cupy",
+    dest="cupy",
+    action="store_true",
+    default=False,
+    help="use Cupy, default is numpy",
+)
+parser.add_argument("--mpi", action="store_true", default=False, help="use mpi")
+parser.add_argument(
+    "--max-frames", type=int, default=0, help="maximum number of frames to process"
+)
+parser.add_argument(
+    "--frames-per-rank",
+    type=int,
+    default=1,
+    help="number of frames per rank to use in each batch",
+)
+parser.add_argument(
+    "--buffer-gather",
+    action="store_true",
+    default=False,
+    help="use buffer objects to gather frames",
+)
+parser.add_argument("--verbose", action="store_true", default=False, help="print info")
+parser.add_argument(
+    "--file-path",
+    type=str,
+    dest="file_path",
+    default="/nobackupp19/chedges/hackday/ccd1/",
+    help="path to directory with files",
+)
+args = parser.parse_args()
+
+# set verbose level fro logger
+if args.verbose:
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# set env variable that will be read by the cupy/numpy importer
+os.environ["USE_CUPY"] = str(args.cupy)
+from scatterbrain.backdrop import BackDrop
+
+# import image loader
+from scatterbrain.cupy_numpy_imports import *
+
+log.info(f"Using {xp.__file__.split('/')[-2]}")
 
 
 def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--cupy",
-        dest="cupy",
-        action="store_true",
-        default=False,
-        help="use Cupy, default is numpy",
-    )
-    parser.add_argument("--mpi", action="store_true", help="use mpi")
-    parser.add_argument(
-        "--max-frames", type=int, default=0, help="maximum number of frames to process"
-    )
-    parser.add_argument(
-        "--frames-per-rank",
-        type=int,
-        default=1,
-        help="number of frames per rank to use in each batch",
-    )
-    parser.add_argument(
-        "--buffer-gather",
-        action="store_true",
-        help="use buffer objects to gather frames",
-    )
-    parser.add_argument("--verbose", action="store_true", help="print info")
-    args = parser.parse_args()
-
-    if args.verbose:
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    # set env variable that will be read by the cupy/numpy importer
-    os.environ["USE_CUPY"] = str(args.cupy)
-    from backdrop import BackDrop
-    # import image loader
-    from cupy_numpy_imports import load_image, load_image_numpy
 
     time_start = time.time()
     if args.mpi:
@@ -66,14 +75,12 @@ def main():
         rank = 0
         size = 1
     time_mpi_init = time.time()
-    if rank == 0:
-        log.info(f"time mpi init: {time_mpi_init-time_start:.4f} s")
+    # if rank == 0:
+    # log.info(f"time mpi init: {time_mpi_init-time_start:.4f} s")
 
     # Initialize list of frames on rank 0
     if rank == 0:
-        # fnames = glob.glob('/nobackupp12/chedges/tess/sector01/camera1/ccd1/*ffic.fits.gz')
-        fnames = glob.glob("/nobackupp19/chedges/hackday/ccd1/*ffic.fits.gz")
-        # fnames = list(range(1234))
+        fnames = glob.glob(f"{args.file_path}/*ffic.fits.gz")
         if args.max_frames > 0:
             fnames = fnames[: args.max_frames]
 
@@ -83,8 +90,8 @@ def main():
     else:
         fnames = None
     time_fnames_init = time.time()
-    if rank == 0:
-        log.info(f"time fnames init: {time_fnames_init-time_mpi_init:.4f} s")
+    # if rank == 0:
+    # log.info(f"time fnames init: {time_fnames_init-time_mpi_init:.4f} s")
 
     # Broadcast filenames to all other ranks
     if comm is not None:
@@ -93,8 +100,8 @@ def main():
         # nothing to do if we are not using mpi
         pass
     time_fnames_bcast = time.time()
-    if rank == 0:
-        log.info(f"time fnames bcast: {time_fnames_bcast-time_fnames_init:.4f} s")
+    # if rank == 0:
+    # log.info(f"time fnames bcast: {time_fnames_bcast-time_fnames_init:.4f} s")
 
     num_files = len(fnames)
     frames_per_rank = args.frames_per_rank
@@ -139,8 +146,8 @@ def main():
         if comm is not None:
             comm.barrier()
         time_batch_load = time.time()
-        if rank == 0:
-            log.info(f"time batch load: {time_batch_load-time_batch_start:.4f} s")
+        # if rank == 0:
+        # log.info(f"time batch load: {time_batch_load-time_batch_start:.4f} s")
 
         # gather frames to rank 0
         if comm is not None:
@@ -162,35 +169,30 @@ def main():
             frames = np.stack(frames)
 
         time_batch_gather = time.time()
-        if rank == 0:
-            log.info(f"time batch gather: {time_batch_gather-time_batch_load:.4f} s")
+        # if rank == 0:
+        # log.info(f"time batch gather: {time_batch_gather-time_batch_load:.4f} s")
 
         if rank == 0:
-            # process frames on rank 0 / perform back drop here
-            # result = np.average(frames, axis=(-2, -1))
-            # log.info(f"{batch_index=} {result}")
-
+            # send to GPU if asked
             if args.cupy:
-                frames = cp.array(frames)
+                frames = xp.array(frames)
 
-            # do backdrop
-            log.info(type(frames))
+            # process frames on rank 0 / perform back drop here
+            log.info(f"frame array is of {type(frames)}")
             b.fit_model(frames)
-            b.save(
-                outfile=f"backdrop_output/backdrop_weights_batch{batch_index:03}.npz"
-            )
+            b.save(outfile=f"../outputs/backdrop_weights_batch{batch_index:03}.npz")
 
-            log.info(f"A      : {type(b.A1.A)}")
-            log.info(f"weights: {type(b.weights_basic[0])}")
+            # log.info(f"A      : {type(b.A1.A)}")
+            # log.info(f"weights: {type(b.weights_basic[0])}")
         else:
             # other ranks have nothing to do
             pass
 
         time_batch_process = time.time()
-        if rank == 0:
-            log.info(
-                f"time batch process: {time_batch_process-time_batch_gather:.4f} s"
-            )
+        # if rank == 0:
+        # log.info(
+        #     f"time batch process: {time_batch_process-time_batch_gather:.4f} s"
+        # )
 
     time_end = time.time()
     if rank == 0:
