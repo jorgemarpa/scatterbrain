4157ab8f0ac9a669e0869d5a316bbf9f5b83c99b
Jorge <jorgemarpa@ug.uchile.cl>
2021-10-04 13:11:22 -0700

    comments and flake8

--- src/run_backdrop_mpi.py
+++ src/run_backdrop_mpi.py
@@ -52,19 +52,20 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# set verbose level fro logger
+# set verbose level for logger
 if args.verbose:
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-# set env variable that will be read by the cupy/numpy importer
-os.environ["USE_CUPY"] = str(args.cupy)
-from scatterbrain import BackDrop
 
-# import image loader
-from scatterbrain.cupy_numpy_imports import load_image_numpy, np, xp
-
-
+# main program
 def main():
+
+    # set env variable that will be read by the cupy/numpy importer
+    os.environ["USE_CUPY"] = str(args.cupy)
+    from scatterbrain import BackDrop
+
+    # import image loader and Numpy/Cupy
+    from scatterbrain.cupy_numpy_imports import load_image_numpy, np, xp
 
     time_start = time.time()
     if args.mpi:
@@ -80,37 +81,42 @@ def main():
         rank = 0
         size = 1
 
-    # Initialize list of frames on rank 0
+    # Initialize list of fits files on rank 0
     if rank == 0:
         log.info(f"Using {xp.__file__.split('/')[-2]}")
         fnames = glob.glob(f"{args.in_dir}/*ffic.fits.gz")
         if args.max_frames > 0:
             fnames = fnames[: args.max_frames]
 
-        # init backdrop in rank 0
+        # init Backdrop object in rank 0
         b = BackDrop()
     else:
         fnames = None
 
-    # Broadcast filenames to all other ranks
+    # Broadcast filenames to all other ranks when using mpi
     if comm is not None:
         fnames = comm.bcast(fnames, root=0)
     else:
         # nothing to do if we are not using mpi
         pass
 
+    # get batch size
     num_files = len(fnames)
     frames_per_rank = args.frames_per_rank
     batch_size = frames_per_rank * size
     # round up in case batch_size does not evenly divide num_files
     num_batches = (num_files + batch_size - 1) // batch_size
+    log.info(f"{num_batches} batches of size {batch_size}")
 
     # read and process files in batches
     for batch_index in range(num_batches):
+        # finding batch start and end accordingly
         batch_start = batch_index * batch_size
         batch_stop = min((batch_index + 1) * batch_size, num_files)
         batch_fnames = fnames[batch_start:batch_stop]
 
+        # if using buffer gather, we need to create buffer variables to load each
+        # batch and to agregate
         if args.buffer_gather:
             sendbuf = np.zeros((frames_per_rank, N, N), dtype=np.float64)
             if rank == 0:
@@ -119,6 +125,7 @@ def main():
                 # other ranks do not need a receive buffer
                 recvbuf = None
         else:
+            # if no buffer gather, we append new items into list and then gather
             frames = []
 
         if rank < len(batch_fnames):
@@ -138,8 +145,8 @@ def main():
             pass
 
         # this barrier is for timing measurement only, other timing statements
-        # do not have this barrrier because they are performed after collective operations
-        # or only relevant to rank 0
+        # do not have this barrrier because they are performed after collective
+        # operations or only relevant to rank 0
         if comm is not None:
             comm.barrier()
 
@@ -166,10 +173,11 @@ def main():
             # send to GPU if asked
             if args.cupy:
                 frames = xp.array(frames)
+            log.info(f"frame array is of {type(frames)}")
             # result = xp.average(frames, axis=(-2, -1))
             # print(f"{batch_index=} {result}")
+
             # process frames on rank 0 / perform back drop here
-            log.info(f"frame array is of {type(frames)}")
             b.fit_model(frames)
             b.save(
                 outfile=f"{args.out_dir}/" f"backdrop_weights_batch{batch_index:03}.npz"
